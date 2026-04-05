c1d923b7bd8fc45d912587aa1e1edd1065aa5cb7
Alex Navarro <alexnavarrosrz@gmail.com>
2026-04-05 13:24:49 -0400

    sql

---
+++ sql/01_creacion_db_tablas.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS empleame_db;
+USE empleame_db;
+/*
+CREATE TABLE tests (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    correo VARCHAR(150) UNIQUE NOT NULL
+);
+
+INSERT INTO tests(correo) VALUES ('hola@gmail.com');
+
+DROP TABLE tests;
+
+SELECT * FROM tests;
+*/
